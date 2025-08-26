d075facd9ee896d1052c1bac934dd4d968d4e57f
Caio Vinícius <caioviniciuscordeirodoamaral@gmail.com>
2025-08-26 19:34:23 -0300

    Configuração e arquitetura inicial do projeto

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-emerald-50"
+    >
+      <Text className="text-3xl font-bold text-purple-800">Hello world!</Text>
+    </View>
+  );
+}
